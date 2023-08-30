245b5cc2cb30cb48bc1a386e150e41bf194886c5
devi-github <devireddyporeddy@.com>
2023-08-30 13:28:54 +0530

    commiting test2

---
+++ src/test/java/devopspackage/Test_02.java
@@ -0,0 +1,12 @@
+package devopspackage;
+
+public class Test_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int x=2;
+  System.out.println(x);
+  
+	}
+
+}
